7d6a25ff186d4cc72100215c616962f1fa65a685
Adith <adithshetty9898@gmail.com>
2018-08-03 20:46:18 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var slideIndex = 0;
 var slides = document.getElementsByClassName("mySlides");
 var slideIndex1 = 0;
 var slides1 = document.getElementsByClassName("mySlides1");
+var slideIndex2 = 0;
+var slides2 = document.getElementsByClassName("mySlides2");
 
 showSlides();
 
@@ -13,7 +15,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+    setTimeout(showSlides, 8000); // Change image every 5 seconds
 }
 
 function currentSlide(no) {
@@ -27,8 +29,16 @@ function currentSlide(no) {
 
 function plusSlides(n) {
   var newslideIndex = slideIndex + n;
-  if(newslideIndex< 4 && newslideIndex > 0){
-     currentSlide(newslideIndex);
+  if(newslideIndex == -1 || newslideIndex == 0){
+    newslideIndex = 3;
+    currentSlide(newslideIndex);
+  }
+  else if(newslideIndex < 4 && newslideIndex > 0){
+   currentSlide(newslideIndex);
+  }
+  else if(newslideIndex=4){
+    newslideIndex = 1;
+    currentSlide(newslideIndex2);
   }
 }
 
@@ -43,11 +53,42 @@ function currentSlide1(no) {
 
 function plusSlides1(n) {
   var newslideIndex1 = slideIndex1 + n;
-  if(newslideIndex1 < 4 && newslideIndex1 > 0){
-     currentSlide1(newslideIndex1);
+  if(newslideIndex1 == -1 || newslideIndex1 == 0){
+    newslideIndex1 = 3;
+    currentSlide1(newslideIndex1);
   }
+  else if(newslideIndex1 < 4 && newslideIndex1 > 0){
+   currentSlide1(newslideIndex1);
+  }
+  else if(newslideIndex1=4){
+    newslideIndex1 = 1;
+    currentSlide1(newslideIndex1);
+  }  
 }
 
+function currentSlide2(no) {
+    var i;    
+    for (i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none"; 
+    }
+    slideIndex2 = no;
+    slides2[no-1].style.display = "block";
+}
+
+function plusSlides2(n) {
+  var newslideIndex2 = slideIndex2 + n;
+  if(newslideIndex2 == -1 || newslideIndex2 == 0){
+      newslideIndex2 = 3;
+      currentSlide2(newslideIndex2);
+  }
+  else if(newslideIndex2 < 4 && newslideIndex2 > 0){
+     currentSlide2(newslideIndex2);
+  }
+  else if(newslideIndex2=4){
+      newslideIndex2 = 1;
+      currentSlide2(newslideIndex2);
+  }
+}
 
 
 
